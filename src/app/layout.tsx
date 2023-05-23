6b4f61e637717e1836ad3ab9636bde90284d364d
hqs <milo.20160809@gmail.com>
2023-05-23 23:40:28 +0800

    fix(script): nextjs script strategy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import './globals.scss'
 
 export const metadata = {
@@ -16,9 +17,9 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.min.css" />
         <link rel="stylesheet" href="https://unpkg.com/leaflet.fullscreen@2.4.0/Control.FullScreen.css" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.min.js"></script>
-        <script src="https://unpkg.com/leaflet.markercluster@1.4.1/dist/leaflet.markercluster.js"></script>
-        <script src="https://unpkg.com/leaflet.fullscreen@2.4.0/Control.FullScreen.js"></script>
+        <Script strategy='beforeInteractive' src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.min.js"></Script>
+        <Script strategy='beforeInteractive' src="https://unpkg.com/leaflet.markercluster@1.4.1/dist/leaflet.markercluster.js"></Script>
+        <Script strategy='beforeInteractive' src="https://unpkg.com/leaflet.fullscreen@2.4.0/Control.FullScreen.js"></Script>
       </head>
       <body>{children}</body>
     </html>
