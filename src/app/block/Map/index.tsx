fd8ed7e8f5261367cc4211c1b1bf7caecde1442a
hqs <milo.20160809@gmail.com>
2023-05-29 15:58:25 +0800

    feat: update tips display

--- src/app/block/Map/index.tsx
+++ src/app/block/Map/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Data } from "@/interface/page";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { renderMapGraph } from "./util";
 import quotes from './quotes.json';
 import styles from './index.module.scss';
@@ -8,23 +8,37 @@ interface MapProps {
   data: Data[]
 }
 
+interface Quote {
+  text: string;
+  medium: string;
+  date: string;
+}
+
+
 const Map: React.FC<MapProps> = ({ data }) => {
+  const [tips, setTips] = useState<Quote>();
   const result = quotes.filter(item => item.text.indexOf('Bitcoin') === -1);
-  const tips = result[Math.floor(Math.random() * result.length)];
+
   useEffect(() => {
     renderMapGraph(data);
+    const index = Math.floor(Math.random() * result.length);
+    setTips(result[index]);
   }, [data]);
 
   return (
     <>
       <div id="mapGraph" className={styles.mapGraph}></div>
-      <p className={styles.tips}>
-        <img src="/people.svg" alt="people" />
-        <div className={styles.content}>
-          { tips.text }
-          <span> —— &quot;Satoshi Nakamoto&quot;, { tips.medium }, { tips.date }</span>
-        </div>
-      </p>
+      {
+        tips && (
+          <p className={styles.tips}>
+            <img src="/people.svg" alt="people" />
+            <div className={styles.content}>
+              { tips.text }
+              <span> —— &quot;Satoshi Nakamoto&quot;, { tips.medium }, { tips.date }</span>
+            </div>
+          </p>
+        )
+      }
     </>
   )
 }
