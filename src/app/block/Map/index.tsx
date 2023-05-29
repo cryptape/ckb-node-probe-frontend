d3e88c1bc939ab6d1400b5f9eab7462e86085515
hqs <milo.20160809@gmail.com>
2023-05-29 15:30:46 +0800

    feat: add quotes

--- src/app/block/Map/index.tsx
+++ src/app/block/Map/index.tsx
@@ -1,20 +1,31 @@
 "use client";
+import { Data } from "@/interface/page";
 import { useEffect } from "react";
 import { renderMapGraph } from "./util";
+import quotes from './quotes.json';
 import styles from './index.module.scss';
-import { Data } from "@/interface/page";
-
 interface MapProps {
   data: Data[]
 }
 
 const Map: React.FC<MapProps> = ({ data }) => {
+  const result = quotes.filter(item => item.text.indexOf('Bitcoin') === -1);
+  const tips = result[Math.floor(Math.random() * result.length)];
   useEffect(() => {
     renderMapGraph(data);
   }, [data]);
 
   return (
-    <div id="mapGraph" className={styles.mapGraph}></div>
+    <>
+      <div id="mapGraph" className={styles.mapGraph}></div>
+      <p className={styles.tips}>
+        <img src="/people.svg" alt="people" />
+        <div className={styles.content}>
+          { tips.text }
+          <span> —— &quot;Satoshi Nakamoto&quot;, { tips.medium }, { tips.date }</span>
+        </div>
+      </p>
+    </>
   )
 }
 
