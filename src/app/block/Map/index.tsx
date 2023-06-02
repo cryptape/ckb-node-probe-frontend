ad73f5ac280e52db145a0f0615b2423607353856
hqs <milo.20160809@gmail.com>
2023-06-02 18:48:27 +0800

    feat: update quotes data and update default fonts

--- src/app/block/Map/index.tsx
+++ src/app/block/Map/index.tsx
@@ -1,40 +1,33 @@
 "use client";
 import { Data } from "@/interface/page";
-import { useEffect, useState } from "react";
 import { renderMapGraph } from "./util";
+import { useEffect, useState } from "react";
+
 import quotes from './quotes.json';
 import styles from './index.module.scss';
 interface MapProps {
   data: Data[]
 }
 
-interface Quote {
-  text: string;
-  medium: string;
-  date: string;
-}
-
-
 const Map: React.FC<MapProps> = ({ data }) => {
-  const [tips, setTips] = useState<Quote>();
-  const result = quotes.filter(item => item.text.indexOf('Bitcoin') === -1);
+  const [text, setText] = useState<string>();
 
   useEffect(() => {
     renderMapGraph(data);
-    const index = Math.floor(Math.random() * result.length);
-    setTips(result[index]);
+    const index = Math.floor(Math.random() * quotes.length);
+    setText(quotes[index]);
   }, [data]);
 
   return (
     <>
       <div id="mapGraph" className={styles.mapGraph}></div>
       {
-        tips && (
+        text && (
           <p className={styles.tips}>
             <img src="/people.svg" alt="people" />
             <div className={styles.content}>
-              { tips.text }
-              <span> —— Satoshi Nakamoto, { tips.medium }, { tips.date }</span>
+              { text }
+              <span> — CKB Whitepaper, 2018</span>
             </div>
           </p>
         )
